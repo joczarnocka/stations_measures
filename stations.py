c52897a88f870216e17c6dfcc5723799e4a4927f
joczarnocka <42293202+joczarnocka@users.noreply.github.com>
2022-08-17 17:58:04 +0200

    add code improvements (including removing pandas data frame iteration)

--- stations.py
+++ stations.py
@@ -1,13 +1,7 @@
-from json import load
 from sqlalchemy import Table, Column, Integer, String, Float, Date, MetaData, ForeignKey
 from sqlalchemy import create_engine
 from sqlalchemy.orm import relationship
 import pandas as pd
-
-# Questions: hot to generalize/simplify load_* functions, how to make FK int instad of station name,
-# Class instead of Table...
-
-engine = create_engine("sqlite:///stations_measures.db")  # , echo=True)
 
 meta = MetaData()
 
@@ -34,88 +28,115 @@ measures = Table(
 )
 
 
-def load_stations() -> None:
+def load_file_to_db(filename, columns, engine, insert_object) -> None:
+    """
+    load stations from csv file fo DB
+    Arguments:
+    - filename
+    - columns - list of columns
+    - engine - database engine
+    - insert_object - object representing table
+    """
+    ins = insert_object.insert()
+
+    df = pd.read_csv(filename, header=0, names=columns)
+
+    dict_records = df.to_dict("records")
+
+    conn = engine.connect()
+    conn.execute(ins, dict_records)
+
+
+def load_stations(filename, engine,) -> None:
     """
     load stations from csv file fo DB
     Arguments:
     -
     """
-    ins = stations.insert()
-    df = pd.read_csv(
-        "clean_stations.csv",
-        header=0,
-        names=[
-            "station",
-            "latitude",
-            "longitude",
-            "elevation",
-            "name",
-            "country",
-            "state",
-        ],
-    )
+    columns = [
+        "station",
+        "latitude",
+        "longitude",
+        "elevation",
+        "name",
+        "country",
+        "state",
+    ]
 
-    ins_records = []
-
-    for index, row in df.iterrows():
-        record = {
-            "station": row["station"],
-            "latitude": row["latitude"],
-            "longitude": row["longitude"],
-            "elevation": row["elevation"],
-            "name": row["name"],
-            "country": row["country"],
-            "state": row["state"],
-        }
-        ins_records.append(record)
-
-    conn = engine.connect()
-    conn.execute(ins, ins_records)
-
-    select1 = stations.select()
-    result = conn.execute(select1)
-
-    print("Stations:\n===========")
-    for row in result:
-        print(row)
+    load_file_to_db(filename, columns, engine, stations)
 
 
-def load_measures() -> None:
+def load_measures(filename, engine) -> None:
     """
     load measures from csv file fo DB
     Arguments:
-    -
+    -filename
+    -DB engine
     """
-    # load measures:
-    ins_measures = measures.insert()
-    df = pd.read_csv(
-        "clean_measure.csv", header=0, names=["station", "date", "precip", "tobs"]
-    )
+    columns = ["station", "date", "precip", "tobs"]
 
-    ins_records = []
+    load_file_to_db(filename, columns, engine, measures)
 
-    for index, row in df.iterrows():
-        record = {
-            "station": row["station"],
-            "date": row["date"],
-            "precip": row["precip"],
-            "tobs": row["tobs"],
-        }
-        ins_records.append(record)
 
+def show_table(select_object, object_name, engine, n) -> None:
+    """
+    printing first n records of a table 
+    Arguments:
+    -select_object
+    -object_name
+    -engine
+    -n - number of printed records
+    """
     conn = engine.connect()
-    conn.execute(ins_measures, ins_records)
-
-    select2 = measures.select().where(measures.c.id <= 10)
-    result = conn.execute(select2)
-
-    print("Measures (first 10):\n===================")
+    select_tmp = select_object.select().limit(n)
+    result = conn.execute(select_tmp)
+    print(f"{object_name}:\n===========")
     for row in result:
         print(row)
 
 
+def show_stations(engine, n=10) -> None:
+    """
+    Print first n records of stations
+    Arguments:
+    -engine - DB engine
+    -n - number of records
+    """
+    show_table(stations, "Stations", engine, n)
+
+
+def show_measures(enginem, n=20) -> None:
+    """
+    Print first n records of mesures
+    Arguments:
+    -engine - DB engine
+    -n - number of records
+    """
+    show_table(measures, "Measures", engine, n)
+
+
+def check_if_data_loaded(engine):
+    conn = engine.connect()
+    select_tmp = stations.select().limit(1)
+    result = conn.execute(select_tmp)
+    for _ in result:
+        return True
+    return False
+
+
 if __name__ == "__main__":
+    engine = create_engine("sqlite:///stations_measures.db")  # , echo=True)
     meta.create_all(engine)
     print(engine.table_names())
-    load_stations()
-    load_measures()
+    loaded = check_if_data_loaded(engine)
+    if loaded:
+        print(
+            "Data had been already loaded - the loading will not be repeated (PK restrictions)"
+        )
+    else:
+        print("Data will be loaded")
+        load_stations("clean_stations.csv", engine)
+        load_measures("clean_measure.csv", engine)
+    print("Data:")
+    show_stations(engine)
+    show_measures(engine)
